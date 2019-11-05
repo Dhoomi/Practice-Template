de570bb2808d41d997be850417000894f95a0711
Dhoom_ <33702504+Dhoomi@users.noreply.github.com>
2019-11-06 00:14:06 +0100

    Update AbstractSerializer.java

--- io/dhoom/util/AbstractSerializer.java
+++ io/dhoom/util/AbstractSerializer.java
@@ -1,7 +1,7 @@
 package io.dhoom.util;
 
-public abstract class AbstractSerializer<T>
-{
+public abstract class AbstractSerializer<T>{
+    
     public abstract String toString(final T p0);
     
     public abstract T fromString(final Object p0);
